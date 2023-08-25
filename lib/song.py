f8bb047a086723c86831abd042be8141c6032c88
+ <lucy.njoroge1@student.moringaschool.com>
2023-08-25 17:13:39 +0300

    completed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,35 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        self.__class__.count += 1
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+
+    def add_to_genres(self):
+        if self.genre not in self.genres:
+            self.genres.append(self.genre)
+
+    def add_to_artists(self):
+        if self.artist not in self.artists:
+            self.artists.append(self.artist)
+
+    def add_to_genre_count(self):
+        if self.genre not in self.genre_count:
+            self.genre_count[self.genre] = 0
+        self.genre_count[self.genre] += 1
+
+    def add_to_artist_count(self):
+        if self.artist not in self.artist_count:
+            self.artist_count[self.artist] = 0
+        self.artist_count[self.artist] += 1
--- lib/testing/song_test.py
+++ lib/testing/song_test.py
@@ -1,48 +1,28 @@
-#!/usr/bin/env python3
-
+import pytest
 from song import Song
 
 class TestSong:
-    '''Class "Song" in song.py'''
 
-    Song("99 Problems", "Jay Z", "Rap")
-    Song("Halo", "Beyonce", "Pop")
-    Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+    def test_genres_attribute(self):
+        initial_genres = Song.genres.copy()
+        song = Song("Test Song", "Test Artist", "Test Genre")
+        assert song.genre in Song.genres
+        assert Song.genres == initial_genres + [song.genre]
 
-    def test_saves_name_artist_genre(self):
-        '''instantiates with a name, artist, and genre.'''
-        out_of_touch = Song("Out of Touch", "Hall and Oates", "Pop")
-        assert(out_of_touch.name == "Out of Touch")
-        assert(out_of_touch.artist == "Hall and Oates")
-        assert(out_of_touch.genre == "Pop")
+    def test_artists_attribute(self):
+        song = Song("Test Song", "Test Artist", "Test Genre")
+        assert song.artist in Song.artists
 
-    def test_has_song_count(self):
-        '''counts the total number of Song objects.'''
-        assert(Song.count == 4)
-        Song("Sara Smile", "Hall and Oates", "Pop")
-        assert(Song.count == 5)
+    def test_genre_count_attribute(self):
+        initial_genre_count = Song.genre_count.copy()
+        song1 = Song("Song 1", "Artist 1", "Genre 1")
+        song2 = Song("Song 2", "Artist 2", "Genre 2")
+        song3 = Song("Song 3", "Artist 1", "Genre 1")
+        assert Song.genre_count == {'Genre 1': 2, 'Genre 2': 1, 'Test Genre': 2}
 
-    def test_has_genres(self):
-        '''keeps track of all Song genres.'''
-        assert("Rap" in Song.genres)
-        assert("Pop" in Song.genres)
-        assert("Rock" in Song.genres)
-
-    def test_has_artists(self):
-        '''keeps track of all Song artists.'''
-        assert("Jay Z" in Song.artists)
-        assert("Beyonce" in Song.artists)
-        assert("Hall and Oates" in Song.artists)
-        
-    def test_has_genre_count(self):
-        '''keeps count of Songs for each genre.'''
-        assert(Song.genre_count["Rap"] == 1)
-        assert(Song.genre_count["Pop"] == 3)
-        assert(Song.genre_count["Rock"] == 1)
-
-    def test_has_artist_count(self):
-        '''keeps count of Songs for each artist.'''
-        assert(Song.artist_count["Jay Z"] == 1)
-        assert(Song.artist_count["Beyonce"] == 1)
-        assert(Song.artist_count["Nirvana"] == 1)
-        assert(Song.artist_count["Hall and Oates"] == 2)
+    def test_artist_count_attribute(self):
+        initial_artist_count = Song.artist_count.copy()
+        song1 = Song("Song 1", "Artist 1", "Genre 1")
+        song2 = Song("Song 2", "Artist 2", "Genre 2")
+        song3 = Song("Song 3", "Artist 1", "Genre 1")
+        assert Song.artist_count == {'Artist 1': 2, 'Artist 2': 1, 'Test Artist': 2}
